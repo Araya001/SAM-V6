d48227db5faa704e90c922871f7cbd1b4a702619
Araya_001 <s5730213001@phuket.psu.ac.th>
2017-05-12 07:37:06 +0700

    Add new version

---
+++ SAM-V6/Models/FileType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAM_V6.Models
+{
+    public enum FileType
+    {
+        Avatar = 1, Photo
+    }
+}
